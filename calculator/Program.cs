fed835234146e56d13ebc34c5445f55e672853ac
Livitha Ravisangar <Livitha.Ravisangar@ksb-sg.ch>
2024-09-01 10:14:11 +0200

    Projektdateien hinzufügen.

---
+++ calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator calc = new calculator();
+
+            Console.WriteLine("Bitte geben Sie den ersten Wert ein:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Bitte geben Sie den zweiten Wert ein:");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Addition: " + calc.Add(a, b));
+            Console.WriteLine("Subtraktion: " + calc.Subtract(a, b));
+            Console.WriteLine("Multiplikation: " + calc.Multiply(a, b));
+            Console.WriteLine("Division: " + calc.Divide(a, b));
+
+            
+        }
+    }
+}
---
+++ calculator/calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class calculator
+    {
+        // Intern gespeicherter Zustand
+        private int lastResult = 0;
+
+        // Methode für Addition
+        public int Add(int a, int b)
+        {
+            lastResult = a + b;
+            return lastResult;
+        }
+
+        // Methode für Subtraktion
+        public int Subtract(int a, int b)
+        {
+            lastResult = a - b;
+            return lastResult;
+        }
+
+        // Methode für Multiplikation
+        public int Multiply(int a, int b)
+        {
+            lastResult = a * b;
+            return lastResult;
+        }
+
+        // Methode für Division
+        public int Divide(int a, int b)
+        {
+            lastResult = a / b;
+            return lastResult;
+        }
+
+        
+    }
+
+
+
+}
+
+
